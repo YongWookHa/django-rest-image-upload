59074d2767a9356200d2939d7382e073a7dfbce1
yongwookha <ywha12@gmail.com>
2020-07-16 16:17:06 +0900

    fix: secret.json file path on BASE_DIR

--- django_rest_imageupload_backend/backend_app/settings.py
+++ django_rest_imageupload_backend/backend_app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 
-with open('secret.json', 'r') as f:
+with open(BASE_DIR/'secret.json', 'r') as f:
     SECRET_KEY = json.loads(f.read())['SECRET_KEY']  # enter your secret key
 
 # SECURITY WARNING: don't run with debug turned on in production!

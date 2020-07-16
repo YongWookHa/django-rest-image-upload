e31cbe1afc08560a394fbd865deeb8cba4fd76bc
yongwookha <ywha12@gmail.com>
2020-07-16 13:39:41 +0900

    add: rest-api base64 image supporting

--- django_rest_imageupload_backend/imageupload_rest/viewsets.py
+++ django_rest_imageupload_backend/imageupload_rest/viewsets.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 
 from imageupload_rest.serializers import UploadedImageSerializer
 from imageupload.models import UploadedImage 
+from user_model_customize.models import User
 from django.core.files.base import ContentFile
 
 import base64
@@ -10,7 +11,7 @@ import numpy as np
 import cv2
 
 def decode_image_from_string(image_string) -> 'numpy image':
-    nparr = np.frombuffer(base64.decodebytes(imgstr.encode('utf8')), np.uint8)
+    nparr = np.frombuffer(base64.decodebytes(image_string.encode('utf8')), np.uint8)
     return cv2.imdecode(nparr, cv2.IMREAD_ANYCOLOR)
 
 class UploadedImagesViewSet(viewsets.ModelViewSet):
@@ -18,10 +19,11 @@ class UploadedImagesViewSet(viewsets.ModelViewSet):
     serializer_class = UploadedImageSerializer
 
     def post(self, request):
-        image_string = request.data.base64
+        image_string = request.data['base64']
         format, imgstr = image_string.split(';base64,')
         ext = format.split('/')[-1]
-        request.data.image = ContentFile(base64.b64decode(imgstr))
+        request.data['image'] = ContentFile(base64.b64decode(imgstr), name='filename.'+ext)
+        request.data['owner'] = User.objects.get(api_key=request.data['api_key']).pk
         
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)

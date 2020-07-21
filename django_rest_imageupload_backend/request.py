3ec3b176847bceaf1cf64087737224f11db07566
yongwookha <ywha12@gmail.com>
2020-07-21 11:16:36 +0900

    add: api version

--- django_rest_imageupload_backend/request.py
+++ django_rest_imageupload_backend/request.py
@@ -10,16 +10,14 @@ if __name__ == "__main__":
         img = f.read()
     
     x = base64.encodebytes(img).decode('utf8')
-    API_HOST = 'http://127.0.0.1:8000/api/base64'
+    API_HOST = 'http://127.0.0.1:8000/api/v1/base64'
 
     inp = {
         # 'data_type' : 'byte-string',
-        'image' : x,
-        'username' : 'diquest',
-        'password' : 'ek2znptm2'
+        'image' : 'data:image/png;base64,'+x,
+        'api_key' : '43102713-18cd-460c-be75-3ab4207dc481',
     }
 
     res = requests.post(API_HOST, data=inp)
     if res:
         print(res.json())
-        print('\n'+res.json()['text'])

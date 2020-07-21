a269faacfc37e2723ea3ddcd2c8f0660fadcf3fd
yongwookha <ywha12@gmail.com>
2020-07-21 11:13:37 +0900

    add: api version

--- django_rest_imageupload_backend/imageupload_rest/urls.py
+++ django_rest_imageupload_backend/imageupload_rest/urls.py
@@ -2,14 +2,13 @@ from django.conf.urls import url, include
 from rest_framework import routers
 from imageupload_rest.viewsets import UploadedImagesViewSet
 
-
-
-router = routers.DefaultRouter()
-router.register('images', UploadedImagesViewSet, 'images')
+# router = routers.DefaultRouter()
+# router.register('images', UploadedImagesViewSet, 'images')
 
 # Wire up our API using automatic URL routing
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'', include(router.urls)),
-    url(r'^base64', UploadedImagesViewSet.as_view({'get': 'list', 'post': 'post'}), name='base64')
+    # url(r'', include(router.urls)),
+    url(r'^images', UploadedImagesViewSet.as_view({'get': 'list', 'post': 'post_images'}), name='images'),
+    url(r'^base64', UploadedImagesViewSet.as_view({'get': 'list', 'post': 'post_base64'}), name='base64')
 ]

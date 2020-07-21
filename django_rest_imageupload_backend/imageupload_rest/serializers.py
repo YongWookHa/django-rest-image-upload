4e09268f80ea0918cafaf33504b34cc1cf5ec28b
yongwookha <ywha12@gmail.com>
2020-07-21 11:12:09 +0900

    fix: write only api key

--- django_rest_imageupload_backend/imageupload_rest/serializers.py
+++ django_rest_imageupload_backend/imageupload_rest/serializers.py
@@ -1,11 +1,19 @@
 from rest_framework import serializers
 from imageupload.models import UploadedImage
+from user_model_customize.models import User
 
 class UploadedImageSerializer(serializers.ModelSerializer):
-    username = serializers.SerializerMethodField("get_username")
+    owner = serializers.SerializerMethodField("get_username")
+    api_key = serializers.CharField(write_only=True)
     class Meta:
         model = UploadedImage
-        fields = ('image_id', 'image', 'username', 'created_date')  # only serialize the primary key and the image field
-    
+        read_only_fields = ['owner', ]
+        fields = ['image_id', 'image', 'owner', 'created_date', 'api_key',]  # only serialize the primary key and the image field
+
+    def create(self, validated_data):
+        validated_data['owner'] = User.objects.get(api_key=validated_data['api_key'])
+        validated_data.pop('api_key', None)
+        return super().create(validated_data)
+
     def get_username(self, obj):
         return obj.owner.username
